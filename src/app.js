583e38b7ab6876ccb0706da596918b4735406a17
ajay rawat <ajrawat92@gmail.com>
2025-02-25 23:05:26 +0530

    Error handling ----route

--- src/app.js
+++ src/app.js
@@ -4,33 +4,56 @@ const {adminAuth,userAuth}=require('./middlewares/auth.js')
 
 const app=express()
 
-app.use('/admin',adminAuth)
-
-app.get('/user',userAuth,(req,res)=>
-{
-res.send("user verified")
-})
-
-app.get('/admin/getAllData',(req,res)=>
-{
-    console.log("dtaa response")
-    res.send("dadta data successfully")
-    
-})
-
-app.get('/admin/deleteData',(eq,res)=>
+app.use('/',(err,req,res,next) =>
     {
-        console.log("delete response")
-        res.send("deleted data successfully")
+        //console.log("some error")
+    if(err)
+      {
+       res.status(500).send("something wen wrong in home ")
+      }
+    });
+  
+
+    // if we  route to getUserdata  the code will run from  top . it will not get any error from upper route('/').now it will get error in ('/getUserData')route 
+  //  which will throw error in lower route('/') which will print the error. 
+  
+  // functionality-----if we have DB error,api error etc. then we have to use try catch block in main code . if  by any means there is some error
+  //then it will  be catch by lower  route 
+
+app.get('/getUserData',(req,res)=>
+{
+    //console.log("inuserdata")
+    //try{
+        throw new Error("cncvnbvc")
+    //res.send("userdata sent")
+
+//     }
+//     catch(err)
+//     {
+//        res.status(500).send("some error kindly contact it dept")
+//    }
     })
 
-    app.get('/user/login',(req,res)=>
-    {
-   res.send(" welcome")
-    })
-    
-    
-    
+
+    app.use('/',(err,req,res,next) =>
+        {
+            //console.log("some error")
+        if(err)
+          {
+           res.status(500).send("something went wrong againnnnnnnnnnn")
+          }
+        });
+
+    // app.use('/',(err,req,res,next) =>
+    //     {
+    //         //console.log("some error")
+    //     if(err)
+    //       {
+    //        res.status(500).send("something wenT  wrong")
+    //       }
+    //     });
+   
+
 
 // app.get('/ab*c',(req,res)=>
 // {
@@ -44,11 +67,26 @@ app.get('/admin/deleteData',(eq,res)=>
 //   res.send('mchvhvJHCHVJCBJBCJBCJBCC')
 // })
 
-// app.use('/test',(req,res)=>
+// app.use('/',(req,res,next)=>
+//      {
+//     //     if(err)
+//     //     {
+//     //         res.status(500).send(" error in home page")
+//     //     }
+//        // res.send("home page")
+//        next()
+//     })
+
+// app.get('/test',(req,res)=>
 //     {
 //      res.send('test creae successfullkjgshjqgcjwbcjby')   
 //     })
    
+
+
+
+
+
 // app.patch('/user',(req,res)=>
 // {
 //     res.send('patch means update the existing record')
