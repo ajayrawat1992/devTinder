5d857ab9ed9e06944af5d7e3a507c961d1a6ef39
ajay rawat <ajrawat92@gmail.com>
2025-03-07 19:10:39 +0530

    Authentication,jwt,cookies

--- src/app.js
+++ src/app.js
@@ -1,96 +1,101 @@
 const express=require('express')
 const {connectDB}=require('./config/Database.js')
-//const {adminAuth,userAuth}=require('./middlewares/auth.js')
+const {userAuth}=require('./middlewares/auth.js')
 const {User}=require('./models/users.js')
 const   {validatorSignUp}=require('./utils/validation.js')
 const bcrypt  = require('bcrypt')
+ const cookieParser=require('cookie-parser')
+ const jwt=require('jsonwebtoken')
 
 const app=express()
 
 app.use(express.json())   //express.json() middleware will convert JSON object into JavaScript object and  adds the javascript object onto req.body 
-
+app.use(cookieParser())  //
 
 
 //get user by emailid
-app.get('/user',async (req,res)=>
-{
-    const userEmail=req.body.emailid
+// app.get('/user',async (req,res)=>
+// {
+//     const userEmail=req.body.emailid
     
-    try {       
-        console.log(userEmail)
-        const user=await User.findOne({emailid:userEmail})
-        if (!user) {
-            res.status(404).send("user not found")
-        }
-       // console.log(user)
-       else{res.send(user)}
+//     try {       
+//         console.log(userEmail)
+//         const user=await User.findOne({emailid:userEmail})
+//         if (!user) {
+//             res.status(404).send("user not found")
+//         }
+//        // console.log(user)
+//        else{res.send(user)}
         
         
-    } catch (error) {
-        console.error('Error: ',error)
-       res.status(400).send(error.message)
-    }
-})
+//     } catch (error) {
+//         console.error('Error: ',error)
+//        res.status(400).send(error.message)
+//     }
+// })
 
-//get user by ID
-app.get('/id',async (req,res)=>
-{
-    try {
-        const userId=req.body._id
-      //  console.log(userId)
-        const  user=await User.findOne({_id:userId})      
-       res.send(user) 
-    } catch (error) {
-       res.status(404).send("user not found")        
-    }
-})
-  // delete user data 
+// //get user by ID
+// app.get('/id',async (req,res)=>
+// {
+//     try {
+//         const userId=req.body._id
+//       //  console.log(userId)
+//         const  user=await User.findOne({_id:userId})      
+//        res.send(user) 
+//     } catch (error) {
+//        res.status(404).send("user not found")        
+//     }
+// })
+//   // delete user data 
 
-app.delete('/user',async(req,res)=>
-    {
-      const userName=req.body.firstName
-      console.log(userName)
-      try {
-        const user=await User.deleteMany({firstName:userName})
-        console.log(user)
-        res.send("data deleted successfully")
-      } catch (error) {
-        res.status(404).send("user not found")
-      }
-    })
+// app.delete('/user',async(req,res)=>
+//     {
+//       const userName=req.body.firstName
+//       console.log(userName)
+//       try {
+//         const user=await User.deleteMany({firstName:userName})
+//         console.log(user)
+//         res.send("data deleted successfully")
+//       } catch (error) {
+//         res.status(404).send("user not found")
+//       }
+//     })
 
 
-//get all the users  from the database
-app.get('/feed',async (req,res)=>
-{
-    try {
-          const users=await User.find({})
-          res.send(users)
-    } catch (error) {
-        console.error('Error: ', error)
-        res.status(400).send(error.message)
+// //get all the users  from the database
+// app.get('/feed',async (req,res)=>
+// {
+//     try {
+//           const users=await User.find({})
+//           res.send(users)
+//     } catch (error) {
+//         console.error('Error: ', error)
+//         res.status(400).send(error.message)
         
-    }
-})
+//     }
+// })
 
 
 //Login 
-   app.post('/login', async(req,res)=>
+app.post('/login', async(req,res)=>
   {
     try {
-
       const {emailid,password}=req.body
      // console.log(emailid)
-const userData= await User.findOne({emailid:emailid})
+     const userData= await User.findOne({emailid:emailid})
 //console.log(userData)
-if(!userData)
-{
-  throw new Error("Invalid credentials")
-}
-const isPasswordValid= await bcrypt.compare(password,userData.password)
-if(isPasswordValid)
-{
-  res.send("Eureka!!!!!")
+     if(!userData)
+    {
+     throw new Error("Invalid credentials")
+    }
+     const isPasswordValid= await bcrypt.compare(password,userData.password)
+    if(isPasswordValid)
+    {
+  const token= jwt.sign({_id:userData._id},'Dev@123',{expiresIn:'7d'})   // expire option in token level
+  console.log('token::: ',token)
+   res.cookie('token',token,{expires: new Date(Date.now() + 1 * 3600000)})   //// expire option in cookie level
+  //console.log(token)
+  res.send("Login successful !!!!!")
 }
 else{
   throw new Error("Invalid credentials")
@@ -122,40 +127,63 @@ app.post('/signup', async (req,res)=>
     }
     })
 
+app.get('/profile',userAuth, async(req,res)=>
+{
+  try {
+      const user=req.user
+      console.log('user........',user)
+    res.send("reading cookies")
+ 
+  } catch (error) {
+    res.status(400).send("error :: "+error.message)
+  }
+  })
+
+
+  app.get('/connectionrequest',userAuth,(req,res)=>
+  {
+    const user=req.user
+
+    console.log("connection making................")
+    res.send(user.firstName+" sent the request")
+  })
+
+
+
 
 // update a data using Patch request
-  app.patch('/user/:userId',async (req,res)=>
-  {    
-    //const userId=req.body._id
-    const userId=req.params?.userId  // req.params.userId is used to retrieve the value of a dynamic parameter (often part of the URL) from a request.  
-    const data=req.body
-    //console.log(data)
-    try {
-        const ALLOWED_UPDATES=['skills','gender']
+//   app.patch('/user/:userId',async (req,res)=>
+//   {    
+//     //const userId=req.body._id
+//     const userId=req.params?.userId  // req.params.userId is used to retrieve the value of a dynamic parameter (often part of the URL) from a request.  
+//     const data=req.body
+//     //console.log(data)
+//     try {
+//         const ALLOWED_UPDATES=['skills','gender']
 
-          const isUpdateAllowed=Object.keys(data).every(k=>ALLOWED_UPDATES.includes(k))
-          console.log(isUpdateAllowed)      // this is API level updation...ex. if we dont want to update emailid 
+//           const isUpdateAllowed=Object.keys(data).every(k=>ALLOWED_UPDATES.includes(k))
+//           console.log(isUpdateAllowed)      // this is API level updation...ex. if we dont want to update emailid 
 
-          if(!isUpdateAllowed)
-          {
-              throw new Error("update not allowed")
-          }
-          if(data?.skills.length > 10)
-          {
-            throw new Error(" Skills Exceeded Maximum length")
-          }
+//           if(!isUpdateAllowed)
+//           {
+//               throw new Error("update not allowed")
+//           }
+//           if(data?.skills.length > 10)
+//           {
+//             throw new Error(" Skills Exceeded Maximum length")
+//           }
          
     
-    const user=await User.findByIdAndUpdate({_id:userId},data,
-       {returnDocument:"after",runValidators:true})      //By default, it will return the document before the update was applied.
-       res.send(user)
+//     const user=await User.findByIdAndUpdate({_id:userId},data,
+//        {returnDocument:"after",runValidators:true})      //By default, it will return the document before the update was applied.
+//        res.send(user)
 
-  } 
-  catch (error) {
-        res.status(404).send("update failed : "+error.message)
-     }
+//   } 
+//   catch (error) {
+//         res.status(404).send("update failed : "+error.message)
+//      }
    
-})
+// })
 
 connectDB()
 .then(()=>
@@ -171,10 +199,3 @@ connectDB()
     console.log("database  cannot be connected")
 })
 
-
-
-//The right way is first we have to connect to the database  ...then start listening to the API calls (start  server )
- //   for ex. the users have started hitting to the API request and the database   has not started yet. So it will create problems
-  // so first connect to the database and then call app.listen().Thats why we have written connectDB function in app.js
-
-  
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,31 +1,36 @@
- const adminAuth=(req,res,next)=>
-    {
-        console.log("home page")
-        const token="xyz"
-        const isAuthorized=token==="xyz"
-        if(!isAuthorized)
-        {
-            res.status(401).send("Unauthorized User")
-        }
-        else{
-            next()
-        }   
-    
-    }
+const jwt=require('jsonwebtoken') 
+const { User } = require('../models/users')
 
-    const userAuth=(req,res,next)=>
+
+    const userAuth= async (req,res,next)=>
         {
-            console.log("home page")
-            const token="xyz"
-            const isAuthorized=token==="xyz"
-            if(!isAuthorized)
+            try {
+
+            const cookies =req.cookies // here we are reading the cookies
+            const {token}=cookies
+
+            if(!token)
             {
-                res.status(401).send("Unauthorized User")
+                throw new Error("Invalid token @@@!!!!!")
             }
-            else{
-                next()
-            }   
+            
+            const decodedObj= jwt.verify(token,'Dev@123')
+            const {_id}=decodedObj
+          // console.log("id  is......",_id)
+            const user= await User.findById(_id)
+
+            if(!user)
+            {
+            throw new Error("user not found")
+            }
+
+            req.user=user
+            next()
+                
+            } catch (error) {
+                res.status(400).send("Error:::"+error.message)
+            }
         
         }
 
-    module.exports={adminAuth,userAuth}
+    module.exports={userAuth}
